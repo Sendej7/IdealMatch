d23dba4c61cd3ef4bb86dd116358dbc36956a4d5
KonradLitwinowicz <56590192+Sendej7@users.noreply.github.com>
2023-02-28 15:53:00 +0100

    Creating API for Login and Register System (SQL)

---
+++ webapi/Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace webapi.Healpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
---
+++ webapi/Helpers/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi.Models;
+namespace webapi.Helpers
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server database
+            options.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
