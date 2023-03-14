c0c54629e28305dd9a227b092d41b4eb9d66465d
KonradLitwinowicz <56590192+Sendej7@users.noreply.github.com>
2023-03-14 11:03:23 +0100

    Adding authorization, login, register

--- reactapp/src/Auth.js
+++ reactapp/src/Auth.js
@@ -1,17 +1,51 @@
 import React, { useState } from "react"
 import './index.css';
 
+async function loginUser(credentials) {
+    return fetch('https://localhost:7135/Accounts/authenticate/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
+ }
+
 export default function (props) {
   let [authMode, setAuthMode] = useState("signin")
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
   const changeAuthMode = () => {
     setAuthMode(authMode === "signin" ? "signup" : "signin")
   }
-
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const response = await loginUser({
+      email,
+      password
+    });
+    if ('refreshToken' in response) {
+      console.log("Success", response.message, "success", {
+        buttons: false,
+        timer: 2000,
+      })
+      .then((value) => {
+        localStorage.setItem('refreshToken', response['refreshToken']);
+        localStorage.setItem('user', JSON.stringify(response['user']));
+        window.location.href = "/";
+      });
+    } else {
+      console.log("Failed", response.message, "error");
+    }
+  }
+  
   if (authMode === "signin") {
     return (
       <div className="Auth-form-container">
-        <form className="Auth-form">
+        <form className="Auth-form" onSubmit={handleSubmit}>
           <div className="Auth-form-content">
             <h3 className="Auth-form-title">Sign In</h3>
             <div className="text-center">
@@ -26,6 +60,7 @@ export default function (props) {
                 type="email"
                 className="form-control mt-1"
                 placeholder="Enter email"
+                onChange={e => setEmail(e.target.value)}
               />
             </div>
             <div className="form-group mt-3">
@@ -34,6 +69,7 @@ export default function (props) {
                 type="password"
                 className="form-control mt-1"
                 placeholder="Enter password"
+                onChange={e => setPassword(e.target.value)}
               />
             </div>
             <div className="d-grid gap-2 mt-3">
